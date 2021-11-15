ae9cc7c649d4ad9df15e418445114ad959f4e95f
Acoleman37 <aleccoleman123@yahoo.com>
2021-11-14 23:26:11 -0800

    added functions

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO Not include",
+        choices: ["strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        title: "The Condition in an if / else statement is enclosed with ________",
+        choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+        answer: "Parenthesis"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store_______",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+        answer: "All of the above"
+    }, 
+    {
+        title: "String values must be enclosed within ______ when being assigned to variables",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer:  "Quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal/Bash", "For Loops", "Consol Log"],
+        answer: "Consol Log"
+    },
+];
